8e54d889c49e86ab15a69a35d79046719bb53b0f
Carlos Lopes <62854228+carlosdevv@users.noreply.github.com>
2024-02-26 21:32:47 -0300

    feat: Events-Exercise

--- Exercises/20-Events-Exercise.sol
+++ Exercises/20-Events-Exercise.sol
@@ -7,21 +7,22 @@ contract EventExample {
     // 👉 user as address type
     // 👉 username as string type
     // CODE HERE 👇
-    
+    event NewUserRegistered(address indexed user, string username);
+
     struct User {
         string username;
         uint256 age;
     }
-    
+
     mapping(address => User) public users;
-    
+
     function registerUser(string memory _username, uint256 _age) public {
         User storage newUser = users[msg.sender];
         newUser.username = _username;
         newUser.age = _age;
-        
+
         // 2️⃣ Emit the event with msg.sender and username as the inputs
         // CODE HERE 👇
-
+        emit NewUserRegistered(msg.sender, _username);
     }
 }
